5c541e83727934c086a67c176dbb8135d1b3cd2b
Joyal George <joyalgeorgepulk@gmail.com>
2023-02-16 01:04:17 +0530

    viewing all categories for user route added

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,21 +9,22 @@ function generateJWT(data){
     return jwt.sign({ tokendata : data }, process.env.PRIVATE_KEY_JWT);
 }
 
-function requiredLogin(req,res,next){
+function validate(req,res,next){
     if(req.headers.token){
         jwt.verify(req.headers.token,process.env.PRIVATE_KEY_JWT,(err,payload)=> {
             if(err){
-                return res.status(401).json({status : false,error : err,message : "Invalid/Expired Token"})
+                return res.status(401).json({status : false,error : err,message : "Invalid/ Expired token"})
             }
+            req.user_id = payload.tokendata.user_id
             next();
         })
     } else{
-        return res.status(401).json({status : false,message : "No Token Found"})
+        return res.status(401).json({status : false,message : "No token found. Access denied"})
     }
 }
 
 module.exports = { 
     comparePassword,
     generateJWT,
-    requiredLogin
+    validate
 }
--- routes/category.js
+++ routes/category.js
@@ -2,11 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 let User = require('../models/User')
-let auth = require('../middlewares/auth')
+let { validate } = require('../middlewares/auth')
 
-router.get('/view-categories', async(req, res) => {
-    console.log("Vieweing user categories");
-    res.send(req.headers.token)
+router.get('/view-categories', validate, async(req, res) => {
+    try {
+        let user_id = req.user_id
+        let result = await User.findOne(
+            { _id : user_id },
+            'categories._id categories.name categories.icon_id categories.color_id'
+        )
+        .populate({ path : 'categories.icon_id', select : 'link' })
+        .populate({ path : 'categories.color_id', select : 'code' })
+        res.status(200).json({ status: true, categories : result.categories })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ status: false, error: error.message, message: "Something went wrong" })
+    }
 });
 
 module.exports = router;
