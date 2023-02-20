10e06614bb18525e2e4ce816637d2f7917aa105c
Joyal George <joyalgeorgepulk@gmail.com>
2023-02-20 17:45:54 +0530

    added plan validation for creating wallet

--- routes/wallet.js
+++ routes/wallet.js
@@ -7,15 +7,20 @@ let { createWalletValidator, validateApp } = require('../validators/wallet-valid
 
 router.post('/create-wallet', validate, createWalletValidator(), validateApp, async (req,res) => {
     try {
-        let new_wallet = new Wallet({
-            name : req.body.name,
-            type : req.body.type,
-            balance : req.body.balance,
-            color_id : req.body.color_id,
-            user_id : req.user_id
-        })
-        let result = await new_wallet.save()
-        res.status(200).json({ status: true, message: "Created wallet", new_wallet: result})
+        let current_wallet_count = await Wallet.count({ user_id: req.user_id });
+        if(req.user_plan.wallets > current_wallet_count){
+            let new_wallet = new Wallet({
+                name : req.body.name,
+                type : req.body.type,
+                balance : req.body.balance,
+                color_id : req.body.color_id,
+                user_id : req.user_id
+            })
+            let result = await new_wallet.save()
+            res.status(200).json({ status: true, message: "Created wallet", new_wallet: result})
+        } else{
+            res.status(423).json({ status: false, message: "Please upgrade your plan" });
+        }
     } catch (error) {
         console.log(error);
         res.status(500).json({ status: false, error: error.message, message: "Something went wrong" })
