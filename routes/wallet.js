39bbad205b077fbd6671c54627293200314527e8
Joyal George <joyalgeorgepulk@gmail.com>
2023-03-12 14:24:18 +0530

    updating wallet feature added

--- routes/wallet.js
+++ routes/wallet.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 let Wallet = require('../models/Wallet')
 let { validate } = require('../middlewares/auth')
-let { createWalletValidator, deleteWalletValidator, validateApp } = require('../validators/wallet-validator')
+let { createWalletValidator, deleteWalletValidator, updateWalletValidator, validateApp } = require('../validators/wallet-validator')
 
 router.post('/create-wallet', validate, createWalletValidator(), validateApp, async (req,res) => {
     try {
@@ -42,6 +42,38 @@ router.delete('/delete-wallet/:wallet_id', validate, deleteWalletValidator(), va
     try {
         await Wallet.findByIdAndDelete(req.params.wallet_id)
         res.status(200).json({ status: true, message: "Deleted wallet"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ status: false, error: error.message, message: "Something went wrong" })
+    }
+})
+
+router.patch('/update-wallet/:wallet_id', validate, updateWalletValidator(), validateApp, async (req,res) => {
+    try {
+        let updateResult = await Wallet.findOneAndUpdate(
+            {
+              _id: req.params.wallet_id,
+              user_id: req.user_id,
+              $or: [
+                { name: { $ne: req.body.name } },
+                { type: { $ne: req.body.type } },
+                { balance: { $ne: req.body.balance } },
+                { color_id: { $ne: req.body.color_id } }
+              ]
+            },
+            {
+              name: req.body.name,
+              type: req.body.type,
+              balance: req.body.balance,
+              color_id: req.body.color_id
+            },
+            { new: true }
+        )
+        if(updateResult){
+            res.status(200).json({ status: true, message: "Wallet updated"})
+        } else{
+            res.status(204).json({ status: false, message: "No changes detected"})
+        }
     } catch (error) {
         console.log(error);
         res.status(500).json({ status: false, error: error.message, message: "Something went wrong" })
--- validators/wallet-validator.js
+++ validators/wallet-validator.js
@@ -61,6 +61,50 @@ const deleteWalletValidator = () => {
     ]
 }
 
+const updateWalletValidator = () => {
+    return [
+        param('wallet_id')
+            .not()
+            .trim()
+            .isEmpty().withMessage('Select a wallet').bail()
+            .isMongoId().withMessage('Invalid wallet id').bail()
+            .custom(async ( value, {req}) => {
+                // checking wallet exists
+                let wallet = await Wallet.findOne({ user_id: req.user_id, _id: value}, '_id')
+                if(!wallet){
+                    return Promise.reject('Wallet not exists')
+                }
+                return true
+            }),
+        body('name')
+            .not()
+            .trim()
+            .isEmpty().withMessage('Enter wallet name').bail(),
+        body('type')
+            .not()
+            .trim()
+            .isEmpty().withMessage('Enter wallet type').bail()
+            .isIn(["Cash", "General", "Savings Account", "Salary Account", "Wallet", "Credit Card"]).withMessage('Invalid wallet type').bail(),
+        body('balance')
+            .not()
+            .trim()
+            .isEmpty().withMessage('Enter current wallet balance').bail()
+            .isNumeric().withMessage('Enter a numeric value').bail(),
+        body('color_id')
+            .not()
+            .trim()
+            .isEmpty().withMessage('Select a color').bail()
+            .isMongoId().withMessage('Invalid color id').bail()
+            .custom(async value => {
+                const color = await Color.findById(value);
+                if (!color) {
+                    return Promise.reject('Color not exists');
+                }
+                return true;
+            })
+    ]
+}
+
 const validateApp = (req, res, next) => {
     const errors = validationResult(req)
     if (errors.isEmpty()) {
@@ -73,5 +117,6 @@ const validateApp = (req, res, next) => {
 module.exports = {
     createWalletValidator,
     deleteWalletValidator,
+    updateWalletValidator,
     validateApp
 }
