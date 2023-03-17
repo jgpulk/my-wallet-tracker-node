36795a9cc5bebedd9940b3fcdbfce29b00ba0524
Joyal George <joyalgeorgepulk@gmail.com>
2023-03-17 18:14:28 +0530

    removed text file logging

--- utils/logger.js
+++ utils/logger.js
@@ -10,14 +10,6 @@ module.exports = createLogger({
               useUnifiedTopology: true
             },
             metaKey: 'metadata' //store addintional data
-        }),
-        new transports.File({ 
-            filename: 'logs/server.log',
-            format: format.combine(
-                format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
-                format.align(),
-                format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
-            )
         })
     ]
 })
